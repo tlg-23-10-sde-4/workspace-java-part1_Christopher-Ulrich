8b70c4e87143913e31ff637eee4593fae088da79
Christopher <christopher.ulrich@tlgcohort.com>
2023-10-31 18:11:39 -0700

    Lab12.1 (replaced BufferedReader with Files class)

--- Lab12.1/src/com/poetry/io/PoemClient.java
+++ Lab12.1/src/com/poetry/io/PoemClient.java
@@ -8,8 +8,9 @@
 
 package com.poetry.io;
 
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class PoemClient {
 
@@ -17,7 +18,7 @@ public class PoemClient {
      * To run one method at a time, uncomment the call to the one you want to execute.
      */
     public static void main(String[] args) {
-        // readPoem();
+        readPoem();
         // writePoem();
     }
 
@@ -34,13 +35,27 @@ public class PoemClient {
      * The try-with-resources below allows you to initialize the stream and auto-close it.
      */
     private static void readPoem() {
-        // TODO: initialize 'reader' variable and complete the try block
-        try (BufferedReader reader = null) {
-
+        // Instead of this BufferedReader null-check for EOF, use Files
+        try {
+            String poem = Files.readString(Path.of("famous-poem.txt"));
+            System.out.println(poem);
         }
         catch (IOException e) {
             e.printStackTrace();
         }
+
+        /*
+        // TODO: initialize 'reader' variable and complete the try block
+        try (BufferedReader reader = new BufferedReader(new FileReader("famous-poem.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {    // null means end of file (EOF)
+                System.out.println(line);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        */
     }
 
     /**
@@ -55,6 +70,19 @@ public class PoemClient {
      * Use a try-with-resources to initialize the stream and auto-close it.
      */
     private static void writePoem() {
-        // TODO
+        String haiku = "The beard is now gone\n" +
+                "What will he be without it? \n" +
+                "Only time will tell";
+
+        // Files.writeString(Path.of("haiku.txt"), haiku);
+
+        try (PrintWriter writer = new PrintWriter(new FileWriter("haiku.txt"))) {
+            writer.println("The beard is now gone");
+            writer.println("What will he be without it?");
+            writer.println("Only time will tell");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
